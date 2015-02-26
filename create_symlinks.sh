5c359937a3e240fe495b8b14d0c00254318b670e
Bogdan Dumitru <dumbogdan@gmail.com>
2015-02-26 14:38:25 +0200

    663 Remove the symlinks before creating

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm dist themes site
 ln -s ../../app/components/shared/lib/semantic-ui/dist dist
 ln -s ../../app/components/shared/lib/semantic-ui/themes themes
 ln -s ../../app/components/shared/lib/semantic-ui/src site
