0172a656c087847fbb45eaf3cd6a6ab4205dbe06
Valentin <blk@griminal.net>
2015-03-09 19:46:15 +0200

    Remove idiotic "-" from script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
--rm dist themes site
+rm dist themes site
 ln -s ../../frontend/components/lib/semantic-ui/dist dist
 ln -s ../../frontend/components/lib/semantic-ui/themes themes
 ln -s ../../frontend/components/lib/semantic-ui/src site
